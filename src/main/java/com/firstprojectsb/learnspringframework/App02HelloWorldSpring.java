553a312513f1cb8f6ae19f1c05ace3fc58687735
Karen Barcelos <karenlbarcelos@hotmail.com>
2024-02-19 18:43:20 -0300

    spring beans concepts

--- src/main/java/com/firstprojectsb/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/firstprojectsb/learnspringframework/App02HelloWorldSpring.java
@@ -3,6 +3,7 @@ package com.firstprojectsb.learnspringframework;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App02HelloWorldSpring {
+
     public static void main(String[] args) {
 
         //1 Launch a Spring Context
@@ -13,5 +14,17 @@ public class App02HelloWorldSpring {
         //3 Retrieving Beans managed by Spring
         System.out.println(context.getBean("name"));  //nome do bean criado - no caso: name
 
+        System.out.println(context.getBean("age"));
+
+        System.out.println(context.getBean("person"));
+
+        System.out.println(context.getBean("address2"));
+
+        //eh possivel acessar um bean passando o tipo do Bean
+        System.out.println(context.getBean(Address.class));
+
+        System.out.println(context.getBean("person2MethodCall"));
+
+        System.out.println(context.getBean("person3Parameters"));
     }
 }
--- src/main/java/com/firstprojectsb/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/firstprojectsb/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,10 @@ package com.firstprojectsb.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//quando usamos record nao eh necessario utilizar getters e setters - cria  automaticamente
+record Person (String name, int age, Address address){}
+record Address (String firstLine, String city){}
+
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -12,4 +16,33 @@ public class HelloWorldConfiguration {
         return "Karen";
     }
 
+    @Bean
+    public int age(){
+        return 24;
+    }
+
+    @Bean //Spring Bean esta gerenciando o objeto "person"
+    public Person person(){
+        return new Person("Karina", 30, new Address("Henry Street", "London"));
+    }
+
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address());
+    }
+
+    @Bean //ao inves de chamar diretamente o metodo, adicionamos parametros
+    public Person person3Parameters(String name, int age, Address address3){
+        return new Person(name, age, address3);  //os parametros sao os nomes dos beans
+    }
+
+    @Bean(name = "address2")
+    public Address address(){
+        return new Address("2, Manor Street", "Dublin");
+    }
+
+    @Bean(name = "address3")
+    public Address address3(){
+        return new Address("Baker Street", "London");
+    }
 }
